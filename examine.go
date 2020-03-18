bff9740f42f9de97646e08d904311b8e8fe3243f
Scott Lester <scott.lester@6point6.co.uk>
2020-03-18 14:33:44 +0000

    first working

---
+++ examine.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"github.com/CaliDog/certstream-go"
+	logging "github.com/op/go-logging"
+)
+
+var log = logging.MustGetLogger("example")
+
+const TYPE_UPDATE = "certificate_update"
+
+var (
+	count_updates int = 0
+)
+
+func main() {
+	// The false flag specifies that we want heartbeat messages.
+	stream, errStream := certstream.CertStreamEventStream(false)
+
+	// catch exit so we can print stats
+	c := make(chan os.Signal)
+    signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	
+	go func() {
+        <-c
+        cleanup(count_updates)
+        os.Exit(1)
+	}()
+
+	for {
+		select {
+			case jq := <-stream:
+				messageType, err := jq.String("message_type")
+
+				if err != nil{
+					log.Fatal("Error decoding jq string")
+				}
+
+				// normally "certificate_update"
+				if messageType == TYPE_UPDATE {
+					count_updates++
+				}
+
+				// dump it
+				log.Info("Message type -> ", messageType)
+				log.Info("recv: ", jq)
+      
+			case err := <-errStream:
+				log.Error(err)
+		}
+	}
+}
+
+// Print stats then exit
+func cleanup(count_updates int) {
+	log.Error("Caught CTL-C. Exiting now\n")
+	log.Info(fmt.Sprintf("Certificates seen: %d", count_updates))
+}
