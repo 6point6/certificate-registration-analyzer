8053c80f4e1be65608e98d9d191233991d3c75f4
Scott Lester <scott.lester@6point6.co.uk>
2020-03-19 15:34:01 +0000

    minor updates

--- examine.go
+++ examine.go
@@ -28,6 +28,8 @@ var (
 type certDetails struct {
 	commonName     string
 	aggregatedName string
+	updateType     string
+	fingerprint    string
 }
 
 func main() {
@@ -65,34 +67,26 @@ func main() {
 	for {
 		select {
 		case jq := <-stream:
-			messageType, err := jq.String("message_type")
-
-			if err != nil {
-				log.Fatal("Error decoding jq string")
-				countErrors++
-			}
-
 			countCertsSeen++
 
-			// normally "certificate_update"
-			if messageType == typeUpdate {
-				countUpdates++
-			}
-
 			// get the details from the map
-			// subject:map[C:<nil> CN:hennieyeh.com L:<nil> O:<nil> OU:<nil> ST:<nil> aggregated:/CN=hennieyeh.com]]
-			subject, err := jq.Object("data", "leaf_cert", "subject")
+			dataMap, err := jq.Object("data")
+			subject, err2 := jq.Object("data", "leaf_cert", "subject")
+			extensions, err3 := jq.Object("data", "leaf_cert")
 
-			if err == nil {
+			if err == nil && err2 == nil && err3 == nil {
 				commonName := fmt.Sprintf("%v", subject["CN"])
 				aggregated := fmt.Sprintf("%v", subject["aggregated"])
+				updateType := fmt.Sprintf("%v", dataMap["update_type"])
+				fingerprint := fmt.Sprintf("%v", extensions["fingerprint"])
 
 				// if in hosepipe mode print all certs
 				if *noFilterPtr {
-					log.Printf("Message type: %q, Subject: %q, Aggregated: %q", messageType, commonName, aggregated)
-				} else if strings.Contains(commonName, *filterPtr) { // else only print matches
-					log.Printf("Message type: %q, Subject: %q, Aggregated: %q", messageType, commonName, aggregated)
-					certificates = append(certificates, certDetails{commonName, aggregated})
+					log.Printf("Type: %q, Subject: %q, Aggregated: %q", updateType, commonName, aggregated)
+				} else if strings.Contains(commonName, *filterPtr) {
+					// else only print matches
+					log.Printf("Type: %q, Subject: %q, Aggregated: %q", updateType, commonName, aggregated)
+					certificates = append(certificates, certDetails{commonName, aggregated, updateType, fingerprint})
 				}
 			} else {
 				log.Println(err)
@@ -116,13 +110,13 @@ func printFinalStats() {
 
 	// print all certs
 	writer := new(tabwriter.Writer)
-	
+
 	// Format in tab-separated columns with a tab stop of 8, padding of 4.
 	writer.Init(os.Stdout, 0, 8, 4, '\t', 0)
-	fmt.Fprintln(writer, "\nSubject\tAggregated\t")
+	fmt.Fprintln(writer, "\nSubject\tAggregated\tUpdate Type\tFingerprint\t")
 
-	for _, cert := range certificates {
-		fmt.Fprintf(writer, "%s\t%s\t\n", cert.commonName, cert.aggregatedName)
+	for i, cert := range certificates {
+		fmt.Fprintf(writer, "%d\t%s\t%s\t%s\t%s\t\n", i, cert.commonName, cert.aggregatedName, cert.updateType, cert.fingerprint)
 	}
 
 	writer.Flush()
